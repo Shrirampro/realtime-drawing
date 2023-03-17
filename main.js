c4cb9cd4b4a535beee420316deeef69a5491f499
Shrirampro <101872044+Shrirampro@users.noreply.github.com>
2023-03-17 20:40:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function setup() 
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup()
 {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -20,11 +25,24 @@ function setup()
     if (results.length > 0)
     {
       console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX = " + noseX +"noseY = " + noseY);
+
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = leftWristX - rightWristX;
+
+      console.log("leftWristX = " + leftWristX + "rightWristX = "+ rightWristX + "difference = " + difference);
     }
  }
 
     function draw () {
       background('#969A97');
+      fill('#F90093');
+      stroke('#F90093');
+      textSize(difference);
+      text("SHRIRAM",50,100);
     }
       
     
